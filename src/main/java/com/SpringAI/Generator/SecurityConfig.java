5abff8b781c5d4492b240f9619fe04fbaafa0115
Tanishq Aryan <100710545+tanishqaryan9@users.noreply.github.com>
2026-07-17 14:06:11 +0530

    Add import for HttpMethod in SecurityConfig

--- src/main/java/com/SpringAI/Generator/SecurityConfig.java
+++ src/main/java/com/SpringAI/Generator/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.http.HttpMethod;
 
 import java.util.List;
 
