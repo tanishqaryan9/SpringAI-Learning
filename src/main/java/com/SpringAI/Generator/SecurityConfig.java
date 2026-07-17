5df83b0634ed04f774d0f6d60fd0b3e4ba6021fb
Tanishq Aryan <aryantanishq123@gmail.com>
2026-07-17 13:06:43 +0530

    trailing forward slash removal

--- src/main/java/com/SpringAI/Generator/SecurityConfig.java
+++ src/main/java/com/SpringAI/Generator/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of(frontendUrl, "https://makeshift-blond.vercel.app/"));
+        config.setAllowedOrigins(List.of(frontendUrl, "https://makeshift-blond.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
